62ab588d5b979be4c132dfdd5cd11ba5b496c4a8
wooclic <wonyul0923@naver.com>
2025-06-05 09:42:28 +0900

    saveToTxt 기능 구현
    수정파일 budget.py

--- OSS_Budget/budget.py
+++ OSS_Budget/budget.py
@@ -24,4 +24,21 @@ class Budget:
         total = sum(e.amount for e in self.expenses)
         print(f"총 지출: {total}원\n")
 
+    def saveToTxt(self, filename="지출목록록.txt"):
+        if not self.expenses:
+            print("지출 내역이 없으므로 저장하지 않습니다.\n")
+            return
+
+        try:
+            with open(filename, "w", encoding="utf-8") as f:
+                f.write("지출 목록록\n")
+                f.write("=" * 30 + "\n")
+                for idx, e in enumerate(self.expenses, 1):
+                    f.write(f"{idx}. {e}\n")
+                f.write("=" * 30 + "\n")
+                total = sum(e.amount for e in self.expenses)
+                f.write(f"총 지출 : {total}원\n")
+            print(f"TXT로 저장 완료! : {filename}\n")
+        except Exception as e:
+            print(f"[오류] TXT 저장 실패 : {e}\n")
 
